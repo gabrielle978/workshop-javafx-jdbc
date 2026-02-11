ccf1c750aacb89ed68bdef77e918c48fc9b94b71
gabrielle978 <gbassiquete@outlook.com>
2026-02-11 17:36:43 -0300

    Local project created

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module br.com.gbass.workshopjavafxjdbc {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens br.com.gbass.workshopjavafxjdbc to javafx.fxml;
+    exports br.com.gbass.workshopjavafxjdbc;
+}
